197ae2ad921d3030d8590af884d53917fdda33fe
macduff <jesse.gomez@wearetopsecret.com>
2013-08-08 10:19:02 -0600

    updates version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyUspsLib',
-    version='0.1.0',
+    version='0.1.1',
     author='Jesse Gomez',
     author_email='jesse@luxnovalabs.com',
     packages=['pyuspslib'],
